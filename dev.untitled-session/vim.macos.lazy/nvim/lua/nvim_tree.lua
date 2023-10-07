511269cca3d5291871d957b6c06c2f681d139382
Andrew <andriyvasyltsiv@gmail.com>
2023-10-07 21:04:10 +0300

    nvim-tree actions added

--- dev.untitled-session/vim.macos.lazy/nvim/lua/nvim_tree.lua
+++ dev.untitled-session/vim.macos.lazy/nvim/lua/nvim_tree.lua
@@ -1,5 +1,9 @@
 local api = require "nvim-tree.api"
 
+local function opts(desc)
+  return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+end
+
 local function vsplit_preview()
   local node = api.tree.get_node_under_cursor()
 
@@ -32,10 +36,6 @@ end
 
 local function my_on_attach(bufnr)
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
   -- default mappings
   api.config.mappings.default_on_attach(bufnr)
 
@@ -49,6 +49,46 @@ local function my_on_attach(bufnr)
   vim.keymap.set("n", "h", api.tree.close,        opts("Close"))
   vim.keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
 end
+
+local git_add = function()
+  local node = api.tree.get_node_under_cursor()
+  local gs = node.git_status.file
+
+  -- If the current node is a directory get children status
+  if gs == nil then
+    gs = (node.git_status.dir.direct ~= nil and node.git_status.dir.direct[1]) 
+         or (node.git_status.dir.indirect ~= nil and node.git_status.dir.indirect[1])
+  end
+
+  -- If the file is untracked, unstaged or partially staged, we stage it
+  if gs == "??" or gs == "MM" or gs == "AM" or gs == " M" then
+    vim.cmd("silent !git add " .. node.absolute_path)
+
+  -- If the file is staged, we unstage
+  elseif gs == "M " or gs == "A " then
+    vim.cmd("silent !git restore --staged " .. node.absolute_path)
+  end
+
+  api.tree.reload()
+end
+
+vim.keymap.set('n', 'ga', git_add, opts('Git Add'))
+
+local function open_tab_silent(node)
+
+  api.node.open.tab(node)
+  vim.cmd.tabprev()
+
+end
+
+vim.keymap.set('n', 'T', open_tab_silent, opts('Open Tab Silent'))
+
+local function change_root_to_global_cwd()
+  local global_cwd = vim.fn.getcwd(-1, -1)
+  api.tree.change_root(global_cwd)
+end
+
+vim.keymap.set('n', '<C-c>', change_root_to_global_cwd, opts('Change Root To Global CWD'))
 
 return {
   my_on_attach = my_on_attach
